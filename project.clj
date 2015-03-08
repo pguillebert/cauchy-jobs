dc182d2d5456162c413e8b9547a256dd6f5c936a
Philippe Guillebert <philippe.guillebert@linkfluence.com>
2015-03-08 22:59:59 +0100

    Looks good

--- project.clj
+++ project.clj
@@ -1,6 +1,20 @@
-(defproject cauchy-jobs "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject cauchy-jobs-kestrel "0.1.0-SNAPSHOT"
+  :description "Cauchy jobs to monitor Kestrel queuing"
+  :url "https://github.com/pguillebert/cauchy-jobs-kestrel"
+  :scm {:name "git"
+        :url "https://github.com/pguillebert/cauchy-jobs-kestrel"}
+  :pom-addition [:developers
+                 [:developer
+                  [:name "Philippe Guillebert"]
+                  [:url "https://github.com/pguillebert"]
+                  [:timezone "+1"]]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :signing {:gpg-key "93FEB8D7"}
+  :deploy-repositories [["clojars" {:creds :gpg}]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.slf4j/slf4j-api "1.7.10"]
+
+                 [cauchy "0.1.0-SNAPSHOT"]
+                 [clj-http "1.0.1"]])
--- src/cauchy/jobs/kestrel.clj
+++ src/cauchy/jobs/kestrel.clj
@@ -1,1 +1,58 @@
-(ns cauchy.jobs.kestrel)
+(ns cauchy.jobs.kestrel
+  (:require [clj-http.client :as http]
+            [cauchy.jobs.utils :as utils]))
+
+(defn fetch-stats
+  [{:keys [host port] :or {host "localhost" port 3334}}]
+  (let [url (str "http://" host ":" port "/stats")]
+    (:body (http/get url {:as :json}))))
+
+(defn all-queues
+  [stats]
+  (->> stats
+      (:gauges)
+      (keys)
+      (map (fn [^clojure.lang.Keyword kw]
+             (let [n (.toString kw)]
+               (re-find #"q\/([a-zA-Z.+-_]+)\/" n))))
+      (map second)
+      (into #{})
+      (remove nil?)))
+
+(defn mk-key
+  [queue metric]
+  (keyword (str "q/" queue "/" metric)))
+
+(defn get-queue-info
+  [stats queue]
+  (let [items (get-in stats [:gauges (mk-key queue "items")])
+        age (get-in stats [:gauges (mk-key queue "age_msec")])
+        put (get-in stats [:counters (mk-key queue "put_items")])
+        get (get-in stats [:counters (mk-key queue "get_items_hit")])
+        put-rate (utils/rate [:kestrel (keyword queue) :put-rate] put)
+        get-rate (utils/rate [:kestrel (keyword queue) :get-rate] get)]
+    {"items" items "age" age
+     "put_rate" put-rate "get_rate" get-rate}))
+
+(def default-thresholds
+  {"age" {:warn 300000 :crit 900000 :comp >}
+   "items" {:warn 10000 :crit 100000 :comp >}
+   "put_rate" {:warn 0.5 :crit 0.01 :comp <}
+   "get_rate" {:warn 0.5 :crit 0.01 :comp <}})
+
+(defn kestrel-stats
+  ([{:keys [thresholds host port]
+     :or {thresholds default-thresholds}
+     :as conf}]
+   (let [stats (fetch-stats conf)
+         infos (->> (all-queues stats)
+                    (map (fn [queue]
+                           [queue
+                            (get-queue-info stats queue)]))
+                    (into {}))]
+     (for [[queue data] infos
+           [sname value] data]
+       {:service (str queue "_" sname)
+        :metric value
+        :state (utils/threshold (get thresholds sname) value)})))
+  ([] (kestrel-stats {})))
